6e2efe9872f0563eb8e885ccf5b79c7619aa9ab3
Marco <30523962+Guray00@users.noreply.github.com>
2020-04-25 01:04:19 +0200

    updates

--- scripts/installer.py
+++ scripts/installer.py
@@ -11,6 +11,7 @@ cache = apt.Cache()
 cache.open()
 
 
+# ARRAYS
 alreadyInstalled = []
 aptPackages = []
 snapPackages = []
@@ -23,7 +24,7 @@ def errorMessage(i):
 	print(colored.red("Something went wrong, i'm sorry..."))
 	errors.append(i)
 
-def MUST4WORK():
+def MUST4WORK():							# Needed dependecies
 	if(not cache["wget"].is_installed):
 		os.system("sudo apt install dpkg-core wget -y")
 	
@@ -34,8 +35,8 @@ def MUST4WORK():
 		os.system("sudo apt install git -y")
 		
 		
-def printResults():
-	pkgs = aptPackages
+def printResults():							# print the result of the program, like
+	pkgs = aptPackages						# installed softwares, errors ecc
 	
 	for i in snapPackages:
 		pkgs.append(i)
@@ -44,18 +45,19 @@ def printResults():
 		pkgs.append(i["name"])
 	
 	if (len(pkgs) > 0 ):	
-		print("\n\n"+colored.green(str(len(pkgs)))+" program had been installed: ")
+		print("\n\n"+colored.green(str(len(pkgs)))+" programs had been installed: ")
 		for i in pkgs:
 			print("   -"+i)
 
 	if (len(alreadyInstalled)>0):	
-			print("\n\n"+colored.blue(str(len(alreadyInstalled)))+" program had been alredy installed: ")
+			print("\n\n"+colored.blue(str(len(alreadyInstalled)))+" programs had been alredy installed: ")
 			for i in alreadyInstalled:
 				print("   -"+i["name"])
 	
 	print("\n")
 			
-def installedCheck(i):
+			
+def installedCheck(i):							# checks if a program is already installed
 	installed = False
 		
 	if(i["method"] != "snap"):
@@ -63,28 +65,32 @@ def installedCheck(i):
 			installed = cache[i["name"]].is_installed
 		except:
 			pass
+	
 	else:
-		if(path.exists(str(Path.home())+"/snap/"+i["name"])):
-			installed = True
+		if(path.exists(str(Path.home())+"/snap/"+i["name"])):	# if the file is a snap, to check if it is installed
+			installed = True				# i need to see the /home/snap folder and check if it is there
 
 	return installed
+
 
 def ppaAdder(i):
 	try:
 		os.system("sudo add-apt-repository ppa:"+i["ppa"]+" -y")
 	except:
 		errorMessage(i)
+
 		
 def sourceAdder(i):
 	try:
 		os.system(i["pgp"])
-		os.system("python3 ./scripts/add_source.py "+ i["deb"] + " "+ i["dest"])
+		os.system("python3 ./scripts/add_source.py "+ i["deb"] + " "+ i["dest"]) 
 		
 	except:
 		errorMessage(i)
 	return
 	
-def checkInstallMethod(i):
+	
+def checkInstallMethod(i):						# checking the installation method (debian)
 	try:
 		method = i["method"] 
 		
@@ -124,15 +130,15 @@ def installerMain(software):
 		installed = installedCheck(i)
 	
 		
-		if (not installed):
+		if (not installed):					# i install only if it is not already installed...				
 			method = checkInstallMethod(i) 
 			if(method == "apt"):
-				aptPackages.append(i["name"])
+				aptPackages.append(i["name"])		# adding the name of the packages to the list of apt
 			
-			elif(method == "snap"):
+			elif(method == "snap"):				# adding the name of the packages to the list of apt
 				snapPackages.append(i["name"])
 			
-			elif(method == "dpkg"):
+			elif(method == "dpkg"):				# installing from file list
 				dpkgPackages.append(i)
 			
 				
@@ -140,10 +146,10 @@ def installerMain(software):
 			alreadyInstalled.append(i)
 		
 	if(len(aptPackages) > 0):
-		os.system("sudo apt update")
+		os.system("sudo apt update")				# apt update is slow, i run it only if i need (so for apt)
 	
 	for i in aptPackages:
-		os.system("sudo apt install " + i)
+		os.system("sudo apt install " + i+" -y")		
 		
 	for i in snapPackages:
 		os.system("sudo snap install "+ i + " --classic")	
