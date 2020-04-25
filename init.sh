b25ccfc7b08492987c1edaf06873fe6a25bf694e
etto48 <minecraft.etto@gmail.com>
2020-04-25 16:28:43 +0200

    Hidden temporary files

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ -e "command.log" ]
+if [ -e ".log" ]
 then
-	rm command.log
+	rm .log
 fi
 
 
 #probe os here
-apt -v >>command.log 2>&1
+apt -v >>.log 2>&1
 if [ $? -eq 0 ]
 then
 	DISTRO=DEBIAN
 fi
-pacman -V >>command.log 2>&1
+pacman -V >>.log 2>&1
 if [ $? -eq 0 ]
 then
 	DISTRO=ARCH
@@ -24,11 +24,11 @@ echo "Detected system type: " $DISTRO
 
 if [ $DISTRO == "DEBIAN" ]
 then
-	dpkg -s "python3" >>command.log 2>&1 
+	dpkg -s "python3" >>.log 2>&1 
 fi
 if [ $DISTRO == "ARCH" ]
 then
-	pacman -Q "python3" >>command.log 2>&1
+	pacman -Q "python3" >>.log 2>&1
 fi
  
 
@@ -39,11 +39,11 @@ then
 	
 		if [ $DISTRO == "DEBIAN" ]
 		then
-			sudo apt install python3 -y >>command.log 2>&1
+			sudo apt install python3 -y >>.log 2>&1
 		fi
 		if [ $DISTRO == "ARCH" ]
 		then
-			yes | sudo pacman -S python3 >>command.log 2>&1
+			yes | sudo pacman -S python3 >>.log 2>&1
 		fi
 
 	else
@@ -51,7 +51,7 @@ then
 fi
 
 
-dpkg -s "python3-pip" >>command.log 2>&1 
+dpkg -s "python3-pip" >>.log 2>&1 
 if [ $? -ne 0 ]
 
 then
@@ -59,11 +59,11 @@ then
 	
 		if [ $DISTRO == "DEBIAN" ]
 		then
-			sudo apt install python3-pip -y >>command.log 2>&1
+			sudo apt install python3-pip -y >>.log 2>&1
 		fi
 		if [ $DISTRO == "ARCH" ]
 		then
-			yes | sudo pacman -S python3-pip >>command.log 2>&1
+			yes | sudo pacman -S python3-pip >>.log 2>&1
 		fi
 	
 	else
@@ -74,33 +74,33 @@ fi
 #sudo apt install python3
 
 
-pip3 install virtualenv >>command.log 2>&1
+pip3 install virtualenv >>.log 2>&1
 
-if [ -d "venv" ]
+if [ -d ".venv" ]
 then
-	echo "Venv found"
-	source ./venv/bin/activate >>command.log 2>&1
+	echo ".venv found"
+	source ./.venv/bin/activate >>.log 2>&1
 else
-	echo "Venv not found, creating now..."
-	virtualenv venv >>command.log 2>&1
-	source ./venv/bin/activate >>command.log 2>&1
-	echo "Updating python venv..."
-	pip3 install -r ./venv-requirements.txt >>command.log 2>&1
+	echo ".venv not found, creating now..."
+	virtualenv .venv >>.log 2>&1
+	source ./.venv/bin/activate >>.log 2>&1
+	echo "Updating python .venv..."
+	pip3 install -r ./.venv-requirements.txt >>.log 2>&1
 
 	if [ $DISTRO == "DEBIAN" ]
 	then
-		echo "Installing apt to venv..."
-		apt download python3-apt >>command.log 2>&1
+		echo "Installing apt to .venv..."
+		apt download python3-apt >>.log 2>&1
 		PYTHONAPT=$(find ./ |grep python3-apt) 
-		dpkg-deb -xv $PYTHONAPT ./ >>command.log 2>&1
-		rm $PYTHONAPT >>command.log 2>&1
-		cp ./usr/lib/python3/dist-packages ./venv/lib/python3.6/ -r >>command.log 2>&1
-		rm -rd ./usr >>command.log 2>&1
+		dpkg-deb -xv $PYTHONAPT ./ >>.log 2>&1
+		rm $PYTHONAPT >>.log 2>&1
+		cp ./usr/lib/python3/dist-packages ./.venv/lib/python3.6/ -r >>.log 2>&1
+		rm -rd ./usr >>.log 2>&1
 	fi
 	if [ $DISTRO == "ARCH" ]
 	then
-		echo "Installing pacman to venv..."
-		pip3 install python-pacman >>command.log 2>&1
+		echo "Installing pacman to .venv..."
+		pip3 install python-pacman >>.log 2>&1
 	fi
 fi
 
@@ -109,7 +109,7 @@ clear
 python3 ./scripts/interface.py --distro $DISTRO
 
 
-deactivate >>command.log 2>&1
+deactivate >>.log 2>&1
 
-#uncomment to clear venv at exit
-#rm -rd ./venv
+#uncomment to clear .venv at exit
+#rm -rd ./.venv
