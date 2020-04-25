e86d4732835eed8aefc4e145de25f431e426615e
etto48 <minecraft.etto@gmail.com>
2020-04-25 16:16:44 +0200

    Python env optimization and command log added

--- init.sh
+++ init.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ -e "command.log" ]
+then
+	rm command.log
+fi
+
+
 #probe os here
-apt -v&>/dev/null
+apt -v >>command.log 2>&1
 if [ $? -eq 0 ]
 then
 	DISTRO=DEBIAN
 fi
-pacman -V&>/dev/null
+pacman -V >>command.log 2>&1
 if [ $? -eq 0 ]
 then
 	DISTRO=ARCH
@@ -18,11 +24,11 @@ echo "Detected system type: " $DISTRO
 
 if [ $DISTRO == "DEBIAN" ]
 then
-	dpkg -s "python3" &> /dev/null 
+	dpkg -s "python3" >>command.log 2>&1 
 fi
 if [ $DISTRO == "ARCH" ]
 then
-	pacman -Q "python3" &> /dev/null
+	pacman -Q "python3" >>command.log 2>&1
 fi
  
 
@@ -33,11 +39,11 @@ then
 	
 		if [ $DISTRO == "DEBIAN" ]
 		then
-			sudo apt install python3 -y
+			sudo apt install python3 -y >>command.log 2>&1
 		fi
 		if [ $DISTRO == "ARCH" ]
 		then
-			yes | sudo pacman -S python3 
+			yes | sudo pacman -S python3 >>command.log 2>&1
 		fi
 
 	else
@@ -45,7 +51,7 @@ then
 fi
 
 
-dpkg -s "python3-pip" &> /dev/null  
+dpkg -s "python3-pip" >>command.log 2>&1 
 if [ $? -ne 0 ]
 
 then
@@ -53,11 +59,11 @@ then
 	
 		if [ $DISTRO == "DEBIAN" ]
 		then
-			sudo apt install python3-pip -y
+			sudo apt install python3-pip -y >>command.log 2>&1
 		fi
 		if [ $DISTRO == "ARCH" ]
 		then
-			yes | sudo pacman -S python3-pip
+			yes | sudo pacman -S python3-pip >>command.log 2>&1
 		fi
 	
 	else
@@ -67,42 +73,43 @@ fi
 
 #sudo apt install python3
 
-#error not redirected for trubleshooting
-pip3 install virtualenv>/dev/null
-source ./linix_venv/bin/activate
 
+pip3 install virtualenv >>command.log 2>&1
 
-echo "Updating python venv..."
-pip3 install -r ./linix_venv/requirements.txt >/dev/null
-
-if [ $DISTRO == "DEBIAN" ]
+if [ -d "venv" ]
 then
-	apt download python3-apt > /dev/null
-	PYTHONAPT=$(find ./ |grep python3-apt)
-	dpkg-deb -xv $PYTHONAPT ./ >/dev/null > /dev/null
-	rm $PYTHONAPT
-	cp ./usr/lib/python3/dist-packages ./linix_venv/lib/python3.6/ -r
-	rm -rd ./usr
-fi
-if [ $DISTRO == "ARCH" ]
-then
-	pip3 install python-pacman >/dev/null
+	echo "Venv found"
+	source ./venv/bin/activate >>command.log 2>&1
+else
+	echo "Venv not found, creating now..."
+	virtualenv venv >>command.log 2>&1
+	source ./venv/bin/activate >>command.log 2>&1
+	echo "Updating python venv..."
+	pip3 install -r ./venv-requirements.txt >>command.log 2>&1
+
+	if [ $DISTRO == "DEBIAN" ]
+	then
+		echo "Installing apt to venv..."
+		apt download python3-apt >>command.log 2>&1
+		PYTHONAPT=$(find ./ |grep python3-apt) 
+		dpkg-deb -xv $PYTHONAPT ./ >>command.log 2>&1
+		rm $PYTHONAPT >>command.log 2>&1
+		cp ./usr/lib/python3/dist-packages ./venv/lib/python3.6/ -r >>command.log 2>&1
+		rm -rd ./usr >>command.log 2>&1
+	fi
+	if [ $DISTRO == "ARCH" ]
+	then
+		echo "Installing pacman to venv..."
+		pip3 install python-pacman >>command.log 2>&1
+	fi
 fi
 
 
 clear
-python3 ./scripts/interface.py $DISTRO
+python3 ./scripts/interface.py --distro $DISTRO
 
-#ONLY FOR DEV
-if [ $DISTRO == "DEBIAN" ]
-then
 
-	rm -rd ./linix_venv/lib/python3.6/dist-packages
+deactivate >>command.log 2>&1
 
-fi
-if [ $DISTRO == "ARCH" ]
-then
-	pip3 uninstall python-pacman >/dev/null
-fi
-
-deactivate
+#uncomment to clear venv at exit
+#rm -rd ./venv
